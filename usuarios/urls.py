23ddc56a9c988e95016c41ac3cf8a3cc5ab6807f
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2023-10-05 21:09:07 -0300

    Solicitar exames

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name="cadastro"),
-    path('login', views.logar, name='logar'),
+    path('login/', views.logar, name='logar'),
+    path('exames/', include('exames.urls')),
 ]
